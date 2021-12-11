5920e7f561a521a87bf18003a4824dae771eb223
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-12-11 06:51:57 +0200

    -

--- main.py
+++ main.py
@@ -75,6 +75,6 @@ def hello():
 
 
 if __name__ == '__main__':
-    socketio.run(app)
-    #http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
-    #http_server.serve_forever()
+    #socketio.run(app)
+    http_server = WSGIServer(('0.0.0.0',5000), app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
